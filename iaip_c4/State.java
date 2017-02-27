0e4fb6ad548f14d8093feac78ff2eb59bb2adbc4
Kristian Mohr Nielsen <Kristian Mohr Nielsen>
2017-02-27 17:26:42 +0100

    heyo again

---
+++ iaip_c4/State.java
@@ -0,0 +1,123 @@
+
+public class State {
+    private int[][] gb;
+    private int[] h;
+
+    private int x, y;
+    private int p;
+    private int latestCol, latestRow, latestP;
+
+    public State(int[][] gb, int[] h, int p){
+        this.x = gb.length;
+        this.y = gb[0].length;
+        this.gb = gb;
+        this.h = h;
+        this.p = p;
+        this.latestP = p;
+    }
+
+    public void put(int a){
+        gb[a][h[a]] = p;
+        latestCol = a;
+        latestRow = h[a];
+        latestP = p;
+        h[a] += 1;
+        p = p == 1 ? 2 : 1;
+    }
+
+    public State clone(){
+        return new State(copy(gb), copy(h), p);
+    }
+
+    private int[][] copy(int[][] board){
+        int[][] arr = new int[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++ ){
+            for(int j = 0; j < board[0].length; j++){
+                arr[i][j] = board[i][j];
+            }
+        }
+        return arr;
+    }
+
+    private int[] copy(int[] board){
+        int[] arr = new int[board.length];
+        for(int i = 0; i < board.length; i++ ){
+            arr[i] = board[i];
+        }
+        return arr;
+    }
+
+    public int isTerminal(){
+        if(checkColumn() || checkRow() || checkDiagLeft() || checkDiagRight()){
+            return latestP;
+        }
+        if(allFull()) return 0;
+        return -1;
+    }
+
+    public boolean isFull(int column){
+        return h[column] >= y;
+    }
+
+    private boolean allFull(){
+        for(int i = 0; i < x; i++){
+            if(!isFull(i)) return false;
+        }
+        return true;
+    }
+
+    private boolean checkColumn(){
+        int tmp = 0;
+        for(int i = 0; i < y; i++){
+            if(gb[latestCol][i] == latestP) {
+                if(++tmp == 4) return true;
+            }
+            else tmp = 0;
+        }
+        return false;
+    }
+
+    private boolean checkRow(){
+        int tmp = 0;
+        for(int i = 0; i < x; i++){
+            if(gb[i][latestRow] == latestP) {
+                if(++tmp == 4) return true;
+            }
+            else tmp = 0;
+        }
+        return false;
+    }
+
+    private boolean checkDiagLeft(){
+        int d = latestCol - latestRow;
+        int tx = d < 0 ? 0 : d;
+        int ty = d < 0 ? -d : 0;
+        int tmp = 0;
+        while(tx < x && ty < y) {
+            if(gb[tx][ty] == latestP) {
+                if(++tmp == 4) return true;
+            }
+            else tmp = 0;
+            tx++;
+            ty++;
+        }
+        return false;
+    }
+
+    private boolean checkDiagRight(){
+        int d = latestCol + latestRow;
+        int diff = d - x + 1;
+        int tx = d >= x ? d - diff : d;
+        int ty = d >= x ? diff : 0;
+        int tmp = 0;
+        while(tx < x && ty < y && tx >= 0) {
+            if(gb[tx][ty] == latestP) {
+                if(++tmp == 4) return true;
+            }
+            else tmp = 0;
+            tx--;
+            ty++;
+        }
+        return false;
+    }
+}
